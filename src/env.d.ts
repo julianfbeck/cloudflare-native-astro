aee2212aa19668f66027d14f63417e929417bb63
Julian Beck <ju-fa-beck@t-online.de>
2024-05-25 18:39:32 +0200

    chore: Update KV namespace key and npm dependencies
    Update the key used to retrieve and store values in the KV namespace in the index.astro file. The previous key was "key" and it has been changed to "key2". This change ensures that the correct value is fetched and stored in the KV namespace.
    
    Also, update the npm dependencies in package.json and pnpm-lock.yaml to include the latest versions of "@cloudflare/workers-types" and "miniflare". Additionally, add the "runtime" dependency for the "@astrojs/cloudflare" package.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,12 @@
 /// <reference types="astro/client" />
 
 type KVNamespace = import("@cloudflare/workers-types").KVNamespace;
+type Queues = import("@cloudflare/workers-types").Queue;
 type ENV = {
   // replace `MY_KV` with your KV namespace
   test: KVNamespace;
+
+  queue: Queues;
 };
 
 // use a default runtime configuration (advanced mode).
@@ -11,3 +14,4 @@ type Runtime = import("@astrojs/cloudflare").Runtime<ENV>;
 declare namespace App {
   interface Locals extends Runtime {}
 }
+
